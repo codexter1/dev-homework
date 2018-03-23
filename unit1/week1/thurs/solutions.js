7e08e1e31a36a6bf6576c16bf67ece5cdd93083f
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-03-22 21:15:38 -0500

    verbal Qs

---
+++ unit1/week1/thurs/solutions.js
@@ -0,0 +1,4 @@
+//verbal questions
+// a parameter creates a space within a function into which an argument can then be inserted. This allows the function to implement on a variety of values at any given time.
+// return commits an output to a function's action. when we use console.log it is the action of console log and not the element that log is printing that is committed.
+ 
