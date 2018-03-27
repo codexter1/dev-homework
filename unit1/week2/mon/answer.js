c2202a956d45554b0dc29fee91da0c7ace011eca
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-03-26 20:50:42 -0500

    created some data structures and accessed some data

--- unit1/week2/mon/answer.js
+++ unit1/week2/mon/answer.js
@@ -4,4 +4,17 @@
 //3. an object holding an array with strings or functions
 //4. an array either by itself or as part of an object.
 //5. an object holding an array of objects with strings
-//6. an object holding an array of objects with arrays 
+//6. an object holding an array of objects with arrays
+
+let rainbow = ['red','orange','yellow','green','blue','indigo','violet'];
+
+console.log(rainbow[4]);
+
+const tommy = {
+  favFood: 'tacos',
+  hobby: 'skateboarding',
+  hometown: 'San Mo',
+  favDataType: 'object',
+}
+
+console.log(tommy.hobby);
