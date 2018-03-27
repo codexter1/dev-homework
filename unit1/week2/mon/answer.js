95c6659a62ae438431633861051a7491931fdb25
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-03-26 20:14:51 -0500

    selecting datatypes

---
+++ unit1/week2/mon/answer.js
@@ -0,0 +1,7 @@
+// datatypes:
+// 1. booleans inside of an object
+//2. a string inside of an object or an array
+//3. an object holding an array with strings or functions
+//4. an array either by itself or as part of an object.
+//5. an object holding an array of objects with strings
+//6. an object holding an array of objects with arrays 
